3c9518359c4922a2d91d137281ad66ddb1cd23db
mattboostkit <matt@boostkit.io>
2025-02-16 12:10:00 +0000

    fix: simplify Instagram feed to use profile embed

--- src/components/InstagramFeed.tsx
+++ src/components/InstagramFeed.tsx
@@ -1,19 +1,6 @@
 "use client";
-import { useEffect } from "react";
 
 export const InstagramFeed = () => {
-  useEffect(() => {
-    // Load Instagram embed script
-    const script = document.createElement("script");
-    script.src = "//www.instagram.com/embed.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <section className="py-12 sm:py-20 bg-white">
       <div className="container mx-auto px-4">
@@ -26,90 +13,14 @@ export const InstagramFeed = () => {
           </p>
         </div>
         
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-          {/* Instagram Post 1 */}
-          <div className="aspect-square w-full">
-            <blockquote 
-              className="instagram-media" 
-              data-instgrm-permalink="https://www.instagram.com/p/C3VXvUuI5Uf/"
-              data-instgrm-version="14"
-              style={{ 
-                background: '#FFF',
-                border: '0',
-                borderRadius: '3px',
-                boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                margin: '1px',
-                maxWidth: '540px',
-                minWidth: '326px',
-                padding: '0',
-                width: 'calc(100% - 2px)'
-              }}
-            >
-            </blockquote>
-          </div>
-          
-          {/* Instagram Post 2 */}
-          <div className="aspect-square w-full">
-            <blockquote 
-              className="instagram-media" 
-              data-instgrm-permalink="https://www.instagram.com/p/C3QfFkxI0Ys/"
-              data-instgrm-version="14"
-              style={{ 
-                background: '#FFF',
-                border: '0',
-                borderRadius: '3px',
-                boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                margin: '1px',
-                maxWidth: '540px',
-                minWidth: '326px',
-                padding: '0',
-                width: 'calc(100% - 2px)'
-              }}
-            >
-            </blockquote>
-          </div>
-          
-          {/* Instagram Post 3 */}
-          <div className="aspect-square w-full hidden lg:block">
-            <blockquote 
-              className="instagram-media" 
-              data-instgrm-permalink="https://www.instagram.com/p/C3LhELzIuFI/"
-              data-instgrm-version="14"
-              style={{ 
-                background: '#FFF',
-                border: '0',
-                borderRadius: '3px',
-                boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                margin: '1px',
-                maxWidth: '540px',
-                minWidth: '326px',
-                padding: '0',
-                width: 'calc(100% - 2px)'
-              }}
-            >
-            </blockquote>
-          </div>
-          
-          {/* Instagram Post 4 */}
-          <div className="aspect-square w-full hidden lg:block">
-            <blockquote 
-              className="instagram-media" 
-              data-instgrm-permalink="https://www.instagram.com/p/C3GiRdJIjpG/"
-              data-instgrm-version="14"
-              style={{ 
-                background: '#FFF',
-                border: '0',
-                borderRadius: '3px',
-                boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                margin: '1px',
-                maxWidth: '540px',
-                minWidth: '326px',
-                padding: '0',
-                width: 'calc(100% - 2px)'
-              }}
-            >
-            </blockquote>
-          </div>
+        <div className="flex justify-center">
+          <iframe
+            src="https://www.instagram.com/limekilnsussex/embed"
+            className="w-full max-w-3xl aspect-square"
+            frameBorder="0"
+            scrolling="no"
+            allowTransparency={true}
+          ></iframe>
         </div>
         
         <div className="text-center mt-8 sm:mt-12">
